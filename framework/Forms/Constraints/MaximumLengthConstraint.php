cde9d17191ae36ae3a9b292117f7820043f7833f
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2016-12-01 15:11:44 +0100

    Added form minimum and maximum length constraints

---
+++ framework/Forms/Constraints/MaximumLengthConstraint.php
@@ -0,0 +1,37 @@
+<?php
+
+/* !
+ * SmoothPHP
+ * This file is part of the SmoothPHP project.
+ * * * *
+ * Copyright (C) 2016 Rens Rikkerink
+ * License: https://github.com/Ikkerens/SmoothPHP/blob/master/License.md
+ * * * *
+ * MinimumLengthConstraint.php
+ * Constraint for forms that requires this particular field to have a minimum length.
+ */
+
+namespace SmoothPHP\Framework\Forms\Constraints;
+
+use SmoothPHP\Framework\Forms\Constraint;
+use SmoothPHP\Framework\Flow\Requests\Request;
+
+class MaximumLengthConstraint extends Constraint {
+    private $maxLength;
+
+    public function __construct($maxLength = 0) {
+        $this->maxLength = $maxLength;
+    }
+
+    public function setAttributes(array &$attributes) {
+        $attributes['attr']['maxlength'] = $this->maxLength;
+    }
+
+    public function checkConstraint(Request $request, $name, $value, array &$failReasons) {
+        if (strlen($value) > $this->maxLength) {
+            global $kernel;
+            $failReasons[] = sprintf($kernel->getLanguageRepository()->getEntry('smooth_form_maxlength'), $name, $this->maxLength);
+        }
+    }
+
+}
---
+++ framework/Forms/Constraints/MinimumLengthConstraint.php
@@ -0,0 +1,37 @@
+<?php
+
+/* !
+ * SmoothPHP
+ * This file is part of the SmoothPHP project.
+ * * * *
+ * Copyright (C) 2016 Rens Rikkerink
+ * License: https://github.com/Ikkerens/SmoothPHP/blob/master/License.md
+ * * * *
+ * MinimumLengthConstraint.php
+ * Constraint for forms that requires this particular field to have a minimum length.
+ */
+
+namespace SmoothPHP\Framework\Forms\Constraints;
+
+use SmoothPHP\Framework\Forms\Constraint;
+use SmoothPHP\Framework\Flow\Requests\Request;
+
+class MinimumLengthConstraint extends Constraint {
+    private $minLength;
+
+    public function __construct($minLength = 0) {
+        $this->minLength = $minLength;
+    }
+
+    public function setAttributes(array &$attributes) {
+        $attributes['attr']['minlength'] = $this->minLength;
+    }
+
+    public function checkConstraint(Request $request, $name, $value, array &$failReasons) {
+        if (strlen($value) < $this->minLength) {
+            global $kernel;
+            $failReasons[] = sprintf($kernel->getLanguageRepository()->getEntry('smooth_form_minlength'), $name, $this->minLength);
+        }
+    }
+
+}
