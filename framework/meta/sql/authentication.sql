8c3ac328f3b9cea6e1e7462b3cd81821d1f14ec7
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2016-12-07 14:46:09 +0100

    Added default authentication sql

---
+++ framework/meta/sql/authentication.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `users` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `loginsession` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `ip` varchar(45) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `state` tinyint(1) NOT NULL,
+  `lastUpdate` int(11) NOT NULL,
+  `failedAttempts` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `sessions` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `selector` varchar(255) NOT NULL,
+  `validator` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `sessions`
+  ADD CONSTRAINT `fk_userid` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
