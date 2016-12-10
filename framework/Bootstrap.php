32967376fc38a7bccbc9a78aecf1920761ceddfd
Rens Rikkerink <github@ikkerens.com>
2016-12-10 14:12:02 +0100

    Renamed production to prod

--- framework/Bootstrap.php
+++ framework/Bootstrap.php
@@ -16,7 +16,7 @@ use SmoothPHP\Framework\Core\Abstracts\WebPrototype;
 use SmoothPHP\Framework\Core\ClassLoader\BasicClassLoader;
 use SmoothPHP\Framework\Cache\Builder\RuntimeCacheProvider;
 
-if (__ENV__ != 'production') {
+if (__ENV__ != 'prod') {
     ini_set('display_errors', '1');
     error_reporting(E_ALL);
 }
