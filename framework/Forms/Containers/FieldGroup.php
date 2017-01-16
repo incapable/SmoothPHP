ead58bf25dfc10425eb308c87be0f254218a480c
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2017-01-16 10:32:16 +0100

    Added grouped form element support

---
+++ framework/Forms/Containers/FieldGroup.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace SmoothPHP\Framework\Forms\Containers;
+
+use SmoothPHP\Framework\Flow\Requests\Request;
+use SmoothPHP\Framework\Forms\Constraint;
+use SmoothPHP\Framework\Forms\Types\StringType;
+
+class FieldGroup extends Type {
+    private $children;
+
+    public function __construct($field) {
+        parent::__construct($field);
+        $this->attributes = array_replace_recursive($this->attributes, array(
+            'children' => array()
+        ));
+    }
+
+    public function initialize(array $attributes) {
+        $childAttributes = $attributes;
+        unset($childAttributes['children']);
+
+        $this->children = array();
+
+        $first = true;
+        foreach ($attributes['children'] as $value) {
+            /* @var $element Type */
+            $element = new $value['type']($value['field']);
+            $element->initialize(array_merge_recursive($childAttributes, $value));
+            $this->children[$value['field']] = new FormContainer(array(
+                'groupseparator' => $first ? '' : '</td></tr><tr><td></td><td>',
+                'input' => $element
+            ));
+            $first = false;
+        }
+    }
+
+    public function getContainer() {
+        return array(
+            'rowstart' => '<tr><td>',
+            'label' => $this->generateLabel(),
+            'rowseparator' => '</td><td>',
+            'children' => new FormContainer($this->children),
+            'rowend' => '</td></tr>'
+        );
+    }
+
+    public function checkConstraint(Request $request, $name, $value, array &$failReasons) {
+        foreach($this->children as $element)
+            if ($element instanceof Constraint) {
+                if ($element instanceof Type)
+                    $value = $request->post->get($element->getFieldName());
+                $element->checkConstraint($request, null, $value, $failReasons);
+            }
+    }
+
+    public static function child($field, $type = null, array $attributes = array()) {
+        return array_merge_recursive(array(
+            'field' => $field,
+            'type' => $type ?: StringType::class,
+            'attr' => array()
+        ), $attributes);
+    }
+
+}
