cfaacd6b0340924bf8f31075e14ae273c712ae08
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2017-02-22 16:57:02 +0100

    Added XML response support

---
+++ framework/Flow/Responses/XML.php
@@ -0,0 +1,98 @@
+<?php
+
+/*!
+ * SmoothPHP
+ * This file is part of the SmoothPHP project.
+ * * * *
+ * Copyright (C) 2016 Rens Rikkerink
+ * License: https://github.com/Ikkerens/SmoothPHP/blob/master/License.md
+ * * * *
+ * XML.php
+ * A response based on XML
+ */
+
+namespace SmoothPHP\Framework\Flow\Responses;
+
+use SmoothPHP\Framework\Core\Kernel;
+use SmoothPHP\Framework\Flow\Requests\Request;
+
+class XML extends Response implements AlternateErrorResponse {
+
+    const XMLNS = '_namespace';
+    const XMLATTR = '_attribute_';
+
+    private $built;
+
+    public function buildErrorResponse($message) {
+        $this->controllerResponse = array(
+            'error' => $message
+        );
+    }
+
+    public function build(Kernel $kernel, Request $request) {
+        $this->built = self::fromArray($this->controllerResponse)->saveXML();
+    }
+
+    protected function sendHeaders() {
+        parent::sendHeaders();
+        header('Content-Type: text/xml; charset=utf-8');
+    }
+
+    protected function sendBody() {
+        echo $this->built;
+    }
+
+    public static function fromArray(array $arrayDoc) {
+        $doc = new \DOMDocument();
+        self::transformArrayToDOMNode($doc, $doc, $arrayDoc);
+        return $doc;
+    }
+
+    private static function transformArrayToDOMNode(\DOMDocument $doc, \DOMNode $parent, array &$array) {
+        foreach($array as $key => $value) {
+            self::createElement($doc, $parent, $key, $value);
+        }
+    }
+
+    private static function createElement(\DOMDocument $doc, \DOMNode $parent, $key, $value) {
+        $elementArguments = array();
+        $method = 'createElement';
+        if (is_array($value) && isset($value['_namespace'])) {
+            $elementArguments[] = $value['_namespace'];
+            $method .= 'NS';
+            unset($value['_namespace']);
+        }
+
+        if (substr($key, -2) != '[]') {
+            $elementArguments[] = $key;
+
+            if (!is_array($value))
+                $elementArguments[] = $value;
+
+            /* @var $element \DOMElement */
+            $element = call_user_func_array(array($doc, $method), $elementArguments);
+
+            if (is_array($value)) {
+                $filteredArray = array_filter($value, function ($value, $key) use (&$element) {
+                    if (substr($key, 0, strlen(self::XMLATTR)) == self::XMLATTR) {
+                        $element->setAttribute(substr($key, strlen(self::XMLATTR)), $value);
+                        return false;
+                    }
+
+                    return true;
+                }, ARRAY_FILTER_USE_BOTH);
+
+                self::transformArrayToDOMNode($doc, $element, $filteredArray);
+            }
+
+            $parent->appendChild($element);
+        } else {
+            $key = substr($key, 0, -2);
+            $elementArguments[] = $key;
+
+            foreach($value as $child)
+                self::createElement($doc, $parent, $key, $child);
+        }
+    }
+
+}
