e410e617dba47e9c42663118eb2756988a2f0c27
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2017-02-24 14:32:20 +0100

    Added HTMLResponse

---
+++ framework/Flow/Responses/HTMLResponse.php
@@ -0,0 +1,33 @@
+<?php
+
+/*!
+ * SmoothPHP
+ * This file is part of the SmoothPHP project.
+ * * * *
+ * Copyright (C) 2016 Rens Rikkerink
+ * License: https://github.com/Ikkerens/SmoothPHP/blob/master/License.md
+ * * * *
+ * HTML.php
+ * A html-based response.
+ */
+
+namespace SmoothPHP\Framework\Flow\Responses;
+
+use SmoothPHP\Framework\Core\Kernel;
+use SmoothPHP\Framework\Flow\Requests\Request;
+
+class HTMLResponse extends Response {
+
+    public function build(Kernel $kernel, Request $request) {
+    }
+
+    protected function sendHeaders() {
+        parent::sendHeaders();
+        header('Content-Type: text/html; charset=utf-8');
+    }
+
+    protected function sendBody() {
+        echo $this->controllerResponse;
+    }
+
+}
