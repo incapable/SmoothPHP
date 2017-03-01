34c93f961bcc5dc531f2158173ec418838bd0f0c
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2017-03-01 14:54:49 +0100

    Added SelectType

---
+++ framework/Forms/Types/SelectType.php
@@ -0,0 +1,55 @@
+<?php
+
+/* !
+ * SmoothPHP
+ * This file is part of the SmoothPHP project.
+ * * * *
+ * Copyright (C) 2016 Rens Rikkerink
+ * License: https://github.com/Ikkerens/SmoothPHP/blob/master/License.md
+ * * * *
+ * SelectType.php
+ * Type for html's select
+ */
+
+namespace SmoothPHP\Framework\Forms\Types;
+
+use SmoothPHP\Framework\Forms\Containers\Type;
+
+class SelectType extends Type {
+
+    const KEY_SELECTOR = 0x1;
+    const VALUE_SELECTOR = 0x2;
+
+    const KEY_ONLY = (self::KEY_SELECTOR << 4) | self::KEY_SELECTOR;
+    const VALUE_ONLY = (self::VALUE_SELECTOR << 4) | self::VALUE_SELECTOR;
+    const KEY_VALUE_PAIR = (self::KEY_SELECTOR << 4) | self::VALUE_SELECTOR;
+    const KEY_VALUE_INVERSE = (self::VALUE_SELECTOR << 4) | self::KEY_SELECTOR;
+
+    public function __construct($field) {
+        parent::__construct($field);
+        $this->attributes = array_replace_recursive($this->attributes, array(
+            'options_mode' => self::KEY_VALUE_PAIR,
+            'options' => array(),
+            'options_attr' => array()
+        ));
+    }
+
+    public function __toString() {
+        try {
+            $mode = last($this->attributes['options_mode']);
+            $options = array();
+            $optionsAttr = $this->transformAttributes($this->attributes['options_attr']);
+
+            foreach ($this->attributes['options'] as $key => $value) {
+                $optionValue = ((($mode >> 4) & self::KEY_SELECTOR) == self::KEY_SELECTOR) ? $key : $value;
+                $labelValue = (($mode & self::KEY_SELECTOR) == self::KEY_SELECTOR) ? $key : $value;
+                $options[] = sprintf('<option value="%s" %s>%s</option>', $optionValue, $optionsAttr, $labelValue);
+            }
+
+            return sprintf('<select %s>%s</select>', $this->transformAttributes($this->attributes['attr']), implode(' ', $options));
+        } catch (\Exception $e) {
+            return '';
+        }
+    }
+
+}
