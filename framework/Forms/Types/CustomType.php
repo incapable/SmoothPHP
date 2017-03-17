a3f835f22cd28e28a7dc1877af4a4fdae88326ce
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2017-03-17 11:17:38 +0100

    Added CustomType to form elements

---
+++ framework/Forms/Types/CustomType.php
@@ -0,0 +1,36 @@
+<?php
+
+/* !
+ * SmoothPHP
+ * This file is part of the SmoothPHP project.
+ * * * *
+ * Copyright (C) 2017 Rens Rikkerink
+ * License: https://github.com/Ikkerens/SmoothPHP/blob/master/License.md
+ * * * *
+ * CustomType.php
+ * Custom input field
+ */
+
+
+namespace SmoothPHP\Framework\Forms\Types;
+
+use SmoothPHP\Framework\Flow\Requests\Request;
+use SmoothPHP\Framework\Forms\Containers\Type;
+
+class CustomType extends Type {
+
+    public function __construct($field) {
+        parent::__construct($field);
+        $this->attributes = array_replace_recursive($this->attributes, array(
+            'required' => false,
+            'content' => '<i>content</i> parameter not set'
+        ));
+    }
+
+    public function checkConstraint(Request $request, $name, $label, $value, array &$failReasons) {}
+
+    public function __toString() {
+        return last($this->attributes['content']);
+    }
+
+}
