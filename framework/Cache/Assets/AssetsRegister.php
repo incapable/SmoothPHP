3ec091af3af6f7c151cdaf40d18c10cdb48e88a2
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2017-03-17 13:53:55 +0100

    Inlining now only happens if the extension is known

--- framework/Cache/Assets/AssetsRegister.php
+++ framework/Cache/Assets/AssetsRegister.php
@@ -130,7 +130,7 @@ class AssetsRegister {
         );
 
         global $kernel;
-        if (__ENV__ != 'dev' && filesize($cachePath) <= $kernel->getConfig()->image_inline_threshold) {
+        if (__ENV__ != 'dev' && isset($mimes[$fileInfo['extension']]) && filesize($cachePath) <= $kernel->getConfig()->image_inline_threshold) {
             return sprintf('data:%s;base64,%s', $mimes[$fileInfo['extension']], base64_encode(file_get_contents($cachePath)));
         } else {
             $virtualPath = sprintf('/images/%s%s.%dx%d.%s',
