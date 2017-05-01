fd555f002f9ff5cce76c643a026fea539e7add7a
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2017-05-01 16:22:06 +0200

    Added permission indexes

--- framework/meta/sql/authentication.sql
+++ framework/meta/sql/authentication.sql
@@ -1,40 +1,51 @@
 CREATE TABLE `users` (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) UNIQUE NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB;
+  `id`       INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `email`    VARCHAR(255) UNIQUE NOT NULL,
+  `password` VARCHAR(255)        NOT NULL
+)
+  ENGINE = InnoDB;
 
 CREATE TABLE `permissions` (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `userId` int(11) DEFAULT NULL,
-  `group` varchar(255) DEFAULT NULL,
-  `permission` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB;
+  `id`         INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `userId`     INT(11)                      DEFAULT NULL,
+  `group`      VARCHAR(255)                 DEFAULT NULL,
+  `permission` VARCHAR(255)                 DEFAULT NULL
+)
+  ENGINE = InnoDB;
 
 CREATE TABLE `loginsessions` (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `ip` varchar(45) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `lastUpdate` int(11) NOT NULL,
-  `failedAttempts` int(11) NOT NULL
-) ENGINE=MEMORY;
+  `id`             INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `ip`             VARCHAR(45)         NOT NULL,
+  `token`          VARCHAR(255)        NOT NULL,
+  `lastUpdate`     INT(11)             NOT NULL,
+  `failedAttempts` INT(11)             NOT NULL
+)
+  ENGINE = MEMORY;
 
 CREATE TABLE `sessions` (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `ip` varchar(50) NOT NULL,
-  `selector` varchar(255) NOT NULL,
-  `validator` varchar(255) NOT NULL
-) ENGINE=MEMORY;
+  `id`         INT(11) PRIMARY KEY                 NOT NULL AUTO_INCREMENT,
+  `userId`     INT(11)                             NOT NULL,
+  `lastActive` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  `ip`         VARCHAR(50)                         NOT NULL,
+  `selector`   VARCHAR(255)                        NOT NULL,
+  `validator`  VARCHAR(255)                        NOT NULL
+)
+  ENGINE = MEMORY;
 
 CREATE TABLE `longlivedsessions` (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `activeSessionId` int(11) NOT NULL,
-  `selector` varchar(255) NOT NULL,
-  `validator` varchar(255) NOT NULL
-) ENGINE=InnoDB;
+  `id`              INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `userId`          INT(11)             NOT NULL,
+  `activeSessionId` INT(11)             NOT NULL,
+  `selector`        VARCHAR(255)        NOT NULL,
+  `validator`       VARCHAR(255)        NOT NULL
+)
+  ENGINE = InnoDB;
 
 ALTER TABLE `permissions`
-  ADD INDEX(`userId`),
-  ADD CONSTRAINT `fk_userid` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD UNIQUE INDEX `userGroup` (`userId`, `group`),
+  ADD UNIQUE INDEX `userPerm` (`userId`, `permission`),
+  ADD UNIQUE INDEX `groupPerm` (`group`, `permission`),
+  ADD INDEX (`userId`),
+  ADD CONSTRAINT `fk_userid` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
