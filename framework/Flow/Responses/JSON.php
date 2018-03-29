f25f09f119072126b79df09129fa7237350dd3d9
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2018-03-29 10:23:34 +0200

    JSON output is now gzipped if supported by the browser

--- framework/Flow/Responses/JSON.php
+++ framework/Flow/Responses/JSON.php
@@ -17,6 +17,7 @@ use SmoothPHP\Framework\Flow\Requests\Request;
 
 class JSON extends Response implements AlternateErrorResponse {
 	private $built;
+	private $gzip;
 
 	public function buildErrorResponse($message) {
 		$this->controllerResponse = [
@@ -29,11 +30,20 @@ class JSON extends Response implements AlternateErrorResponse {
 		$this->built = json_encode($this->controllerResponse);
 		if ($this->built === false)
 			throw new \RuntimeException('Could not encode json: ' . json_last_error_msg());
+
+		if (strpos($request->server->HTTP_ACCEPT_ENCODING, 'gzip') !== false) {
+			$this->gzip = true;
+			$this->built = gzencode($this->built);
+		}
 	}
 
 	protected function sendHeaders() {
 		parent::sendHeaders();
 		header('Content-Type: application/json; charset=utf-8');
+		header('Vary: Accept-Encoding');
+
+		if ($this->gzip)
+			header('Content-Encoding: gzip');
 	}
 
 	protected function sendBody() {
