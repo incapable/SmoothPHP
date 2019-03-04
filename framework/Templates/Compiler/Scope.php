2a82dfafc984a4ee8f1532daccd2c53d817ddc99
Rens Rikkerink <rens@eastbridge.eu>
2019-03-04 12:04:54 +0100

    Refactored Scope

--- framework/Templates/Compiler/Scope.php
+++ framework/Templates/Compiler/Scope.php
@@ -13,37 +13,22 @@
 namespace SmoothPHP\Framework\Templates\Compiler;
 
 class Scope {
-	private $parent;
 	private $variables;
 
-	public function __construct(Scope $parent = null) {
-		$this->parent = $parent;
+	public function __construct(Scope &$parent = null) {
 		$this->variables = [];
+		if ($parent != null)
+			foreach ($parent->variables as $key => &$value)
+				$this->variables[$key] = &$value;
 	}
 
 	public function __set($name, $value) {
-		if (!$this->setIfDeclared($name, $value)) {
-			$this->variables[$name] = $value;
-		}
-	}
-
-	private function setIfDeclared($name, $value) {
-		if (isset($this->variables[$name])) {
-			$this->variables[$name] = $value;
-			return true;
-		} else {
-			if ($this->parent == null)
-				return false;
-			else
-				return $this->parent->setIfDeclared($name, $value);
-		}
+		$this->variables[$name] = $value;
 	}
 
 	public function __get($name) {
 		if (isset($this->variables[$name]))
 			return $this->variables[$name];
-		else if ($this->parent != null)
-			return $this->parent->{$name};
 		else
 			return null;
 	}
