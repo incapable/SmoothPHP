723c367b17cf48cb0e7ea1557b0e3eb841f6acb7
Rens Rikkerink <rens@eastbridge.eu>
2019-04-16 13:10:38 +0200

    Improvements in __ROOT__ determination

--- framework/Bootstrap.php
+++ framework/Bootstrap.php
@@ -26,7 +26,7 @@ set_error_handler(function ($severity, $msg, $file, $line) {
 });
 
 if (!defined('__ROOT__'))
-	define('__ROOT__', str_replace('public', '', $_SERVER['DOCUMENT_ROOT']));
+	define('__ROOT__', strrev(preg_replace(strrev('/public/'), '', strrev($_SERVER['DOCUMENT_ROOT']), 1)));
 
 require_once __ROOT__ . 'framework/Core/Utilities.php';
 require_once __ROOT__ . 'framework/Core/ClassLoader/BasicClassLoader.php';
